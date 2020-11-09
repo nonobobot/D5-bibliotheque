deebf703f6d7cde01fc2ae05cd1a3af6bc7757f9
nonobobot <44499442+nonobobot@users.noreply.github.com>
2020-11-09 14:24:57 +0100

    commit09

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const titreSpans = document.querrySelectorAll('h1 span');
+
+window.addEventListener('load', () => {
+const TL = gsap.timeline(paused:true});
+
+TL.staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+
+TL.play();
+})
